ed3e235decc9a91b8e5fccc06de9361e102f5032
Amar_Asfaw <amarasfaw1077@gmail.com>
2024-09-21 01:17:01 +0300

    finished the auth

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { connection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./router/userRoutes.js";
+import auctionItemRouter from "./router/auctionItemRoutes.js";
+import bidRouter from "./router/bidRoutes.js";
+import commissionRouter from "./router/commissionRouter.js";
+import superAdminRouter from "./router/superAdminRoutes.js";
+import { endedAuctionCron } from "./automation/endedAuctionCron.js";
+import { verifyCommissionCron } from "./automation/verifyCommissionCron.js";
+
+const app = express();
+dotenv.config();
+
+
+app.use(
+  cors({
+    origin: [ 'http://localhost:5174'], // Explicitly allow your frontend's origin
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true, // This allows cookies and other credentials to be sent
+  })
+);
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auctionitem", auctionItemRouter);
+app.use("/api/v1/bid", bidRouter);
+app.use("/api/v1/commission", commissionRouter);
+app.use("/api/v1/superadmin", superAdminRouter);
+
+endedAuctionCron();
+verifyCommissionCron();
+connection();
+app.use(errorMiddleware);
+
+export default app;
